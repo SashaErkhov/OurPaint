c4bee5db4b3a8487ef22f1a50d870723920a8113
77maxikov <77maxikov@mail.ru>
2024-03-12 10:42:12 +0300

    getElement and save|load

--- paint.h
+++ paint.h
@@ -6,14 +6,31 @@
 #include "objects.h"
 
 enum Element{ET_POINT, ET_SECTION, ET_CIRCLE};
+
+struct ElementData{
+    Element et;
+    Arry<double> params;
+};
+
 class Paint
 {
     Arry<point> pointar;
     Arry<section> sectionar;
     Arry<circle> circlear;
 public:
-    //Добавление элементов с указанием их типа и необходимого набора параментов
-    void addElement(Element et, const Arry<double>& params);
+    //Добавление элементов с указанием их типа и необходимого набора параметров
+    ID addElement(const ElementData& ed);
+
+    // Получение информации об объекте    
+    ElementData getElementInfo(ID id);
+     
+
+    // Сохранение данных в файл
+    void saveToFile(const char* filename);
+
+    // Загрузка данных из файла
+    void loadFromFile(const char* filename);
+
     //Отрисовка текущего состояния
     void paint();
 
