81a1544367e147cd83d2fb707c7b619d216e4cf4
Ivan Ovsyukov <st117301@student.spbu.ru>
2024-04-09 11:51:15 +0300

    Добавил = для id

--- objects.cpp
+++ objects.cpp
@@ -1,5 +1,12 @@
 #include "objects.h"
 
+ID& ID::operator=(const ID& x){
+    if(this!=&x){
+        id=x.id;
+    }
+    return *this;
+}
+
 bool operator>(const ID& left, const ID& right){
   if(left.id<right.id){
     return false;
