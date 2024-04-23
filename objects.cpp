83d3fe7f975ba9733699e0526a894b1e4b8e0322
Eugene Bychkov <118733164+Ardrass2@users.noreply.github.com>
2024-04-23 11:00:59 +0300

    Add rectangle realization

--- objects.cpp
+++ objects.cpp
@@ -39,3 +39,26 @@ std::ifstream& operator>> (std::ifstream& in, point& x) {
     in >> x.y;
     return in;
 }
+
+rectangle point::rect() {
+    rectangle r = {x, y, x, y};
+    return r;
+}
+rectangle circle::rect() {
+    rectangle r = {center->x - R, center->y - R, center->x + R, center->y + R};
+    return r;
+}
+rectangle section::rect() {
+    rectangle r = {beg->x, beg->y, end->x, end->y};
+    return r;
+}
+
+rectangle rectangle::operator||(const rectangle &r) {
+    rectangle r1 = {
+        std::min(x_1, r.x_1),
+        std::min(y_1, r.y_1),
+        std::max(x_2, r.x_2),
+        std::max(y_2, r.y_2)
+    };
+    return r1;
+}
