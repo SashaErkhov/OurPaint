4fae8cd41011a03ea4dd8af10cbbaa1c3b64f90c
Eugene Bychkov <118733164+Ardrass2@users.noreply.github.com>
2024-04-23 11:00:32 +0300

    Add rectangle, create methods rect to figures

--- objects.h
+++ objects.h
@@ -1,7 +1,7 @@
 #pragma once
 #ifndef MRETOR
 #define MRETOR
-
+#include <fstream>
 struct ID {
 	long long int id;
 	ID(long long int i = 0) : id(i) {}
@@ -11,24 +11,32 @@ struct ID {
 bool operator>(const ID& left, const ID& right);
 bool operator<(const ID& left, const ID& right);
 bool operator==(const ID& left, const ID& right);
-
 std::ifstream& operator>> (std::ifstream& in, ID& x);
+
+struct rectangle {
+        double x_1;
+        double x_2;
+        double y_1;
+        double y_2;
+        rectangle operator||(const rectangle& r);
+};
 
 struct point {
 	double x;
 	double y;
+    rectangle rect();
 };
-
 std::ifstream& operator>> (std::ifstream& in, point& x);
-
 struct section {
 	point* beg;
 	point* end;
+    rectangle rect();
 };
 
 struct circle {
 	point* center;
 	double R;
+    rectangle rect();
 };
 
 #endif
