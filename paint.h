dcdb779e6f15c55b4fb1fe4f921a8b809db673ab
Ibragim Oskanov <Oskanov-ibragim.06@mail.ru>
2024-05-09 16:07:30 +0300

    Add getDerivative function

--- paint.h
+++ paint.h
@@ -2,6 +2,8 @@
 #ifndef PAINT_23
 #define PAINT_23
 
+#define PARAMID double*
+
 #include "BMPfile.h"
 #include "Arry.h"
 #include "List.h"
@@ -14,12 +16,117 @@ enum Element {
     ET_POINT, ET_SECTION, ET_CIRCLE
 };
 
+enum Requirement {
+    ET_POINTONPOINT,
+    ET_POINTPOINTDIST,
+    ET_POINTONSECTION,
+    ET_POINTSECTIONDIST,
+    ET_SECTIONCIRCLEDIST,
+    ET_SECTIONSECTIONDISTPARALLEL,
+    ET_SECTIONSECTIONPERPENDICULAR,
+    ET_SECTIONSECTIONANGEL,
+    ET_SECTIONSECTIONANGELEQ,
+    ET_SECTIONONCIRCLE,
+    ET_SECTIONINCIRCLE
+};
+
+struct IReq {
+    virtual double getError() = 0;
+    virtual Arry<PARAMID> getParams() = 0;
+    virtual double getDerivative(PARAMID) = 0;
+};
+
+
+class ReqPointSegDist :public IReq {
+    point* m_pt;
+    section* m_sect;
+
+public:
+    virtual double getError();
+    virtual Arry<PARAMID> getParams() {
+        Arry<PARAMID> pars;
+        pars.addElement(&m_pt->x);
+        pars.addElement(&m_pt->y);
+        pars.addElement(&m_sect->beg->x);
+        pars.addElement(&m_sect->beg->y);
+        pars.addElement(&m_sect->end->x);
+        pars.addElement(&m_sect->end->y);
+        return pars;;
+    };
+    virtual double getDerivative(PARAMID param) {
+        double x0 = m_pt->x;
+        double y0 = m_pt->y;
+
+        double x1 = m_sect->beg->x;
+        double y1 = m_sect->beg->y;
+
+        double x2 = m_sect->end->x;
+        double y2 = m_sect->end->y;
+
+        if (param == &m_pt->x) { // x0
+            double num = (-y1 + y2) * (-((-x1 + x2) * y0) + x2 * y1 - x1 * y2 + x0 * (-y1 + y2));
+            double den = sqrt(pow((-((-x1 + x2) * y0) + x2 * y1 - x1 * y2 + x0 * (-y1 + y2)), 2))
+                * sqrt(pow(-x1 + x2, 2) + pow(-y1 + y2, 2));
+
+            return num / den;
+        }
+        else if (param == &m_pt->y) { // y0
+            double num = (x1 - x2) * (-((-x1 + x2) * y0) + x2 * y1 - x1 * y2 + x0 * (-y1 + y2));
+            double den = sqrt(pow((-((-x1 + x2) * y0) + x2 * y1 - x1 * y2 + x0 * (-y1 + y2)), 2))
+                * sqrt(pow((-x1 + x2), 2) + pow((-y1 + y2), 2));
+
+            return num / den;
+        }
+        else if (param == &m_sect->beg->x) { // x1
+            double num1 = (-x1 + x2) * sqrt(pow((-((-x1 + x2) * y0) + x2 * y1 - x1 * y2 + x0 * (-y1 + y2)), 2));
+            double den1 = sqrt(pow((pow((-x1 + x2), 2) + pow((-y1 + y2), 2)), 3));
+            double num2 = (y0 - y2) * (-((-x1 + x2) * y0) + x2 * y1 - x1 * y2 + x0 * (-y1 + y2));
+            double den2 = sqrt(pow((-((-x1 + x2) * y0) + x2 * y1 - x1 * y2 +
+                x0 * (-y1 + y2)), 2)) * sqrt(pow((-x1 + x2), 2) + pow((-y1 + y2), 2));
+
+            return num1 / den1 + num2 / den2;
+        }
+        else if (param == &m_sect->beg->x) { // y1
+            double num1 = (-y1 + y2) * sqrt(pow((-((-x1 + x2) * y0) + x2 * y1 - x1 * y2 + x0 * (-y1 + y2)), 2));
+            double den1 = sqrt(pow((pow((-x1 + x2), 2) + pow((-y1 + y2), 2)), 3));
+            double num2 = (-x0 + x2) * (-((-x1 + x2) * y0) + x2 * y1 - x1 * y2 + x0 * (-y1 + y2));
+            double den2 = sqrt(pow((-((-x1 + x2) * y0) + x2 * y1 - x1 * y2 +
+                x0 * (-y1 + y2)), 2)) * sqrt(pow((-x1 + x2), 2) + pow((-y1 + y2), 2));
+
+            return num1 / den1 + num2 / den2;
+        }
+        else if (param == &m_sect->end->x) { // x2
+            double num1 = -(-x1 + x2) * sqrt(pow((-((-x1 + x2) * y0) + x2 * y1 - x1 * y2 + x0 * (-y1 + y2)), 2));
+            double den1 = sqrt(pow((pow((-x1 + x2), 2) + pow((-y1 + y2), 2)), 3));
+            double num2 = (-y0 + y1) * (-((-x1 + x2) * y0) + x2 * y1 - x1 * y2 + x0 * (-y1 + y2));
+            double den2 = sqrt(pow((-((-x1 + x2) * y0) + x2 * y1 - x1 * y2 +
+                x0 * (-y1 + y2)), 2)) * sqrt(pow((-x1 + x2), 2) + pow((-y1 + y2), 2));
+
+            return num1 / den1 + num2 / den2;
+        }
+        else if (param == &m_sect->end->x) { // y2
+            double num1 = -(-y1 + y2) * sqrt(pow((-((-x1 + x2) * y0) + x2 * y1 - x1 * y2 + x0 * (-y1 + y2)), 2));
+            double den1 = sqrt(pow((pow((-x1 + x2), 2) + pow((-y1 + y2), 2)), 3));
+            double num2 = (x0 - x1) * (-((-x1 + x2) * y0) + x2 * y1 - x1 * y2 + x0 * (-y1 + y2));
+            double den2 = sqrt(pow((-((-x1 + x2) * y0) + x2 * y1 - x1 * y2 +
+                x0 * (-y1 + y2)), 2)) * sqrt(pow((-x1 + x2), 2) + pow((-y1 + y2), 2));
+
+            return num1 / den1 + num2 / den2;
+        }
+
+        return 0;
+    };
+};
+
+
+
 struct ElementData {
     Element et;
     Arry<double> params;
 
     ElementData();
 };
+
 //c_ - класс, v_- переменная, s_структура, m_ - контейнеры(списки, массивы и другие)
 class Paint {
     Assoc<ID, List<point>::iterator> m_pointIDs;
