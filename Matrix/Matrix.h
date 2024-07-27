8c39398145b57c96c3b5f5f8e292a1b335114fd7
Ibragim Oskanov <Oskanov-ibragim.06@mail.ru>
2024-07-27 10:26:06 +0300

    Fix matrix and added transpose function

--- Matrix/Matrix.h
+++ Matrix/Matrix.h
@@ -2,14 +2,14 @@
 #define MATRIX
 
 #include <iostream>
+#include <stdexcept>
 
-///////////////////////////// general purpose functions
 template <typename T>
 class Matrix
 {
 public:
     Matrix(unsigned int rowsI, unsigned int colsI) : rows(rowsI), cols(colsI) {
-        matrix = new T* [rows];
+        matrix = new T * [rows];
         for (size_t i = 0; i < rows; i++) {
             matrix[i] = new T[cols];
             for (size_t j = 0; j < cols; j++) {
@@ -17,72 +17,9 @@ public:
             }
         }
     }
-    Matrix &operator*=(const Matrix &other) {
-        if (rows == 1 && cols == 1){
-            Matrix newMatrix(other.rows, other.cols);
-            for (int i = 0; i < other.rows; i++) {
-                for (int j = 0; j < other.cols; j++) {
-                    newMatrix.matrix[i][j] = matrix[0][0] * other.matrix[i][j];
-                }
-            }
-            matrix = newMatrix.matrix;
-            rows = other.rows;
-            cols = other.cols;
-            return *this;
-        }
-        if (other.rows == 1 && other.cols == 1){
-            for (int i = 0; i < other.rows; i++) {
-                for (int j = 0; j < other.cols; j++) {
-                    matrix[i][j] *= other.matrix[0][0];
-                }
-            }
-            return *this;
-        }
-        if (cols != other.rows) {
-            throw std::invalid_argument("Matrices must have the same size");
-        }
-        Matrix newMatrix(rows, other.cols);
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < other.cols; j++) {
-                newMatrix.matrix[i][j] = 0;
-                for (int k = 0; k < other.rows; k++) {
-                    newMatrix.matrix[i][j] += matrix[i][k] * other.matrix[k][j];
-                }
-            }
-        }
-        matrix = newMatrix.matrix;
-        rows = newMatrix.rows;
-        cols = newMatrix.cols;
-        return *this;
-    }
-    //some operators for computing
-    Matrix &operator+=(const Matrix &other) {
-        if (rows != other.rows || cols != other.cols) {
-            throw std::invalid_argument("Matrices must have the same size");
-        }
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                matrix[i][j] += other.matrix[i][j];
-            }
-        }
-        return *this;
-    }
-
-    Matrix &operator-=(const Matrix &other) {
-        if (rows != other.rows || cols != other.cols) {
-            throw std::invalid_argument("Matrices must have the same size");
-        }
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                matrix[i][j] -= other.matrix[i][j];
-            }
-        }
-        return *this;
-    }
-    //------------------------------------------------------------------------------
 
     Matrix(const Matrix& other) : rows(other.rows), cols(other.cols) {
-        matrix = new T* [rows];
+        matrix = new T * [rows];
         for (size_t i = 0; i < rows; i++) {
             matrix[i] = new T[cols];
             for (size_t j = 0; j < cols; j++) {
@@ -101,7 +38,7 @@ public:
             rows = other.rows;
             cols = other.cols;
 
-            matrix = new T* [rows];
+            matrix = new T * [rows];
             for (size_t i = 0; i < rows; i++) {
                 matrix[i] = new T[cols];
                 for (size_t j = 0; j < cols; j++) {
@@ -112,18 +49,88 @@ public:
         return *this;
     }
 
+    //~Matrix() {
+    //    for (size_t i = 0; i < rows; i++) {
+    //        delete[] matrix[i];
+    //    }
+    //    delete[] matrix;
+    //}
 
-    ~Matrix()
-    {
-        for (size_t i = 0; i < rows; i++)
-        {
+    Matrix transpose() const {
+        Matrix transposed(cols, rows);
+        for (size_t i = 0; i < rows; ++i) {
+            for (size_t j = 0; j < cols; ++j) {
+                transposed.matrix[j][i] = matrix[i][j];
+            }
+        }
+        return transposed;
+    }
+
+    Matrix& operator*=(const Matrix& other) {
+        if (cols != other.rows) {
+            throw std::invalid_argument("Matrices must have compatible dimensions for multiplication.");
+        }
+
+        Matrix newMatrix(rows, other.cols);
+        for (size_t i = 0; i < rows; i++) {
+            for (size_t j = 0; j < other.cols; j++) {
+                newMatrix.matrix[i][j] = 0;
+                for (size_t k = 0; k < cols; k++) {
+                    newMatrix.matrix[i][j] += matrix[i][k] * other.matrix[k][j];
+                }
+            }
+        }
+
+        for (size_t i = 0; i < rows; i++) {
             delete[] matrix[i];
         }
         delete[] matrix;
+
+        matrix = newMatrix.matrix;
+        newMatrix.matrix = nullptr;
+
+        rows = newMatrix.rows;
+        cols = newMatrix.cols;
+
+        return *this;
     }
-    [[nodiscard]] inline unsigned int rows_size()  const  {return rows;}
-    [[nodiscard]] inline unsigned int cols_size()  const   {return cols;}
-    // Method to set elements in the matrix
+
+    Matrix& operator*=(T scalar) {
+        for (size_t i = 0; i < rows; i++) {
+            for (size_t j = 0; j < cols; j++) {
+                matrix[i][j] *= scalar;
+            }
+        }
+        return *this;
+    }
+
+    Matrix& operator+=(const Matrix& other) {
+        if (rows != other.rows || cols != other.cols) {
+            throw std::invalid_argument("Matrices must have the same size for addition.");
+        }
+        for (size_t i = 0; i < rows; i++) {
+            for (size_t j = 0; j < cols; j++) {
+                matrix[i][j] += other.matrix[i][j];
+            }
+        }
+        return *this;
+    }
+
+    Matrix& operator-=(const Matrix& other) {
+        if (rows != other.rows || cols != other.cols) {
+            throw std::invalid_argument("Matrices must have the same size for subtraction.");
+        }
+        for (size_t i = 0; i < rows; i++) {
+            for (size_t j = 0; j < cols; j++) {
+                matrix[i][j] -= other.matrix[i][j];
+            }
+        }
+        return *this;
+    }
+
+    [[nodiscard]] inline unsigned int rows_size() const { return rows; }
+    [[nodiscard]] inline unsigned int cols_size() const { return cols; }
+
     void setElement(unsigned int rowI, unsigned int colI, T value) {
         if (rowI >= rows || colI >= cols) {
             throw std::out_of_range("Index out of range");
@@ -131,7 +138,6 @@ public:
         matrix[rowI][colI] = value;
     }
 
-    // Method to get elements from the matrix
     T getElement(unsigned int rowI, unsigned int colI) const {
         if (rowI >= rows || colI >= cols) {
             throw std::out_of_range("Index out of range");
@@ -149,7 +155,7 @@ public:
             throw std::runtime_error("Matrix is singular and cannot be inverted.");
         }
 
-        T** invMat = new T* [rows];
+        T** invMat = new T * [rows];
         for (size_t i = 0; i < rows; ++i) {
             invMat[i] = new T[cols];
         }
@@ -173,19 +179,6 @@ private:
     unsigned int rows;
     unsigned int cols;
 
-    // comparing strings
-    bool compareStrings(const char* str1, const char* str2) {
-        size_t i = 0;
-        while (str1[i] != '\0' && str2[i] != '\0') {
-            if (str1[i] != str2[i]) {
-                return false;
-            }
-            i++;
-        }
-        return str1[i] == str2[i];
-    }
-
-    // Function to free matrix memory
     void freeMatrix(T** matrix, int size) {
         for (size_t i = 0; i < size; ++i) {
             delete[] matrix[i];
@@ -193,103 +186,31 @@ private:
         delete[] matrix;
     }
 
-    ///////////////////////////// functions for computing coefficients of the characteristic polynomial
-
-    // Function to multiply two matrices
-    T** um(T** a, T** b, int size) {
-        T** c = new T* [size];
-        for (int i = 0; i < size; i++)
-        {
-            c[i] = new T[size];
-            for (int j = 0; j < size; j++)
-            {
-                c[i][j] = 0;
-                for (int k = 0; k < size; k++) {
-                    c[i][j] += a[i][k] * b[k][j];
-                }
-            }
-        }
-        return c;
-    }
-
-    // precomputing matrix to the highest required power and saving it
-    void precomputePowers(T** matrix, int size, T*** powers, int maxExp) {
-        powers[0] = new T* [size];
-        for (int i = 0; i < size; ++i) {
-            powers[0][i] = new T[size];
-            for (int j = 0; j < size; ++j) {
-                powers[0][i][j] = 0.0; // Filling with zeros
-            }
-            powers[0][i][i] = 1.0; // Setting ones on the diagonal
-        }
-
-        powers[1] = new T* [size];
-        for (int i = 0; i < size; ++i) {
-            powers[1][i] = new T[size];
-            for (int j = 0; j < size; ++j) {
-                powers[1][i][j] = matrix[i][j]; // Original matrix for power 1
-            }
-        }
-
-        for (int exp = 2; exp <= maxExp; ++exp) {
-            powers[exp] = um(powers[exp - 1], matrix, size);
-        }
-    }
-
-    // trace of a matrix raised to a power
-    T trace(T*** powers, int size, int n) {
-        T sum = 0;
-        for (int i = 0; i < size; ++i) {
-            sum += powers[n][i][i];
-        }
-        return sum;
-    }
-
-    // coefficients of the characteristic polynomial with index k
-    T coef(T*** powers, int N, int k, T* coefCache, bool* isComputed) {
-        if (k == 0) {
-            return 1.0;
-        }
-
-        if (isComputed[k]) {
-            return coefCache[k];
-        }
-
-        T result = -trace(powers, N, k);
-        for (int i = 1; i < k; ++i) {
-            result -= coef(powers, N, i, coefCache, isComputed) * trace(powers, N, k - i);
-        }
-        result /= k;
-
-        coefCache[k] = result;
-        isComputed[k] = true;
-
-
-
-        return result;
-    }
-
-    ///////////////////////////// functions for computing the inverse matrix
-
-    // Calculating the inverse matrix
     void inverseMatrix(T** matrix, T** inverse, int size) {
-        // Creating an identity matrix
+        // Создание копии исходной матрицы, чтобы не изменять ее
+        T** tempMatrix = new T * [size];
+        for (size_t i = 0; i < size; ++i) {
+            tempMatrix[i] = new T[size];
+            for (size_t j = 0; j < size; ++j) {
+                tempMatrix[i][j] = matrix[i][j];
+            }
+        }
+
+        // Создание единичной матрицы
         for (size_t i = 0; i < size; ++i) {
             for (size_t j = 0; j < size; ++j) {
                 inverse[i][j] = (i == j) ? 1.0 : 0.0;
             }
         }
 
-        // Applying Gauss-Jordan elimination
+        // Применение метода Гаусса-Жордана
         for (size_t i = 0; i < size; ++i) {
-            // Ensuring the element matrix[i][i] is non-zero
-            if (matrix[i][i] == 0.0) {
-                // Searching for a non-zero element in the current column
+            // Обеспечение ненулевого элемента на диагонали
+            if (tempMatrix[i][i] == 0.0) {
                 for (size_t j = i + 1; j < size; ++j) {
-                    if (matrix[j][i] != 0.0) {
-                        // Swapping rows in both matrices
+                    if (tempMatrix[j][i] != 0.0) {
                         for (size_t k = 0; k < size; ++k) {
-                            std::swap(matrix[i][k], matrix[j][k]);
+                            std::swap(tempMatrix[i][k], tempMatrix[j][k]);
                             std::swap(inverse[i][k], inverse[j][k]);
                         }
                         break;
@@ -297,32 +218,31 @@ private:
                 }
             }
 
-            // Normalizing the row so that element matrix[i][i] equals 1
-            T factor = matrix[i][i];
+            // Нормализация строки так, чтобы диагональный элемент стал равен 1
+            T factor = tempMatrix[i][i];
             for (size_t j = 0; j < size; ++j) {
-                matrix[i][j] /= factor;
+                tempMatrix[i][j] /= factor;
                 inverse[i][j] /= factor;
             }
 
-            // Nullifying all elements in the current column
+            // Обнуление всех элементов в текущем столбце, кроме диагонального
             for (size_t j = 0; j < size; ++j) {
                 if (j != i) {
-                    factor = matrix[j][i];
+                    factor = tempMatrix[j][i];
                     for (size_t k = 0; k < size; ++k) {
-                        matrix[j][k] -= factor * matrix[i][k];
+                        tempMatrix[j][k] -= factor * tempMatrix[i][k];
                         inverse[j][k] -= factor * inverse[i][k];
                     }
                 }
             }
         }
+
+        // Очистка временной матрицы
+        freeMatrix(tempMatrix, size);
     }
 
-    ///////////////////////////// functions for computing the determinant
-
-    // determinant by Gauss method
     T det(T** matrix, int s) {
-        // Creating a copy of the matrix
-        T** tempMatrix = new T* [s];
+        T** tempMatrix = new T * [s];
         for (size_t i = 0; i < s; ++i) {
             tempMatrix[i] = new T[s];
             for (size_t j = 0; j < s; ++j) {
@@ -333,27 +253,21 @@ private:
         T d = 1;
         T eps = 1e-10;
         for (size_t i = 0; i < s; ++i) {
-            if (tempMatrix[i][i] < eps && tempMatrix[i][i] > -eps) {
-                int k;
-                for (k = i + 1; k < s; ++k) {
-                    if (tempMatrix[k][i] != 0) {
-                        // Swapping rows
+            if (std::abs(tempMatrix[i][i]) < eps) {
+                for (size_t k = i + 1; k < s; ++k) {
+                    if (std::abs(tempMatrix[k][i]) > eps) {
                         for (size_t j = 0; j < s; ++j) {
-                            T temp = tempMatrix[i][j];
-                            tempMatrix[i][j] = tempMatrix[k][j];
-                            tempMatrix[k][j] = temp;
+                            std::swap(tempMatrix[i][j], tempMatrix[k][j]);
                         }
                         d *= -1;
                         break;
                     }
                 }
-                if (k == s) {
-                    freeMatrix(tempMatrix, s);
-                    // all zero
-                    return 0;
-                }
             }
-            // Reducing matrix to upper triangular form
+            if (std::abs(tempMatrix[i][i]) < eps) {
+                freeMatrix(tempMatrix, s);
+                return 0;
+            }
             for (size_t k = i + 1; k < s; ++k) {
                 T factor = tempMatrix[k][i] / tempMatrix[i][i];
                 for (size_t j = i; j < s; ++j) {
@@ -368,28 +282,30 @@ private:
         return d;
     }
 };
+
 template <typename T>
-Matrix<T> operator*(const Matrix<T> &matrix1, const Matrix<T> &matrix2) {
-    Matrix result(matrix1);
+Matrix<T> operator*(const Matrix<T>& matrix1, const Matrix<T>& matrix2) {
+    Matrix<T> result(matrix1);
     return result *= matrix2;
 }
 
 template <typename T>
-Matrix<T> operator+(const Matrix<T> &matrix1, const Matrix<T> &matrix2) {
+Matrix<T> operator+(const Matrix<T>& matrix1, const Matrix<T>& matrix2) {
     if (matrix1.rows_size() != matrix2.rows_size() ||
         matrix1.cols_size() != matrix2.cols_size()) {
-        throw std::invalid_argument("Matrices have different sizes");
+        throw std::invalid_argument("Matrices must have the same size for addition.");
     }
-    Matrix result(matrix1);
+    Matrix<T> result(matrix1);
     return result += matrix2;
 }
+
 template <typename T>
-Matrix<T> operator-(const Matrix<T> &matrix1, const Matrix<T> &matrix2) {
+Matrix<T> operator-(const Matrix<T>& matrix1, const Matrix<T>& matrix2) {
     if (matrix1.rows_size() != matrix2.rows_size() ||
         matrix1.cols_size() != matrix2.cols_size()) {
-        throw std::invalid_argument("Matrices have different sizes");
+        throw std::invalid_argument("Matrices must have the same size for subtraction.");
     }
-    Matrix result(matrix1);
+    Matrix<T> result(matrix1);
     return result -= matrix2;
 }
 
