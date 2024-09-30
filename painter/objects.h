48e84b9168af7367915e365c63c16ef14facd009
ardrass <st117289@student.spbu.ru>
2024-10-01 00:17:36 +0300

    added pure virutal class for primitives. In view of this the returned types of elements are added

--- painter/objects.h
+++ painter/objects.h
@@ -1,42 +1,70 @@
 #pragma once
 #ifndef MRETOR
 #define MRETOR
+
 #include <fstream>
+#include "enums.h"
+
 struct ID {
-	long long int id;
-	ID(long long int i = 0) : id(i) {}
-	ID& operator=(const ID& x);
+    long long int id;
+
+    ID(long long int i = 0) : id(i) {}
+
+    ID &operator=(const ID &x);
 };
 
-bool operator>(const ID& left, const ID& right);
-bool operator<(const ID& left, const ID& right);
-bool operator==(const ID& left, const ID& right);
-std::ifstream& operator>> (std::ifstream& in, ID& x);
+bool operator>(const ID &left, const ID &right);
+
+bool operator<(const ID &left, const ID &right);
+
+bool operator==(const ID &left, const ID &right);
+
+std::ifstream &operator>>(std::ifstream &in, ID &x);
 
 struct rectangle {
-        double x_1;
-        double x_2;
-        double y_1;
-        double y_2;
-        rectangle operator||(const rectangle& r);
+    double x_1;
+    double x_2;
+    double y_1;
+    double y_2;
+
+    rectangle operator||(const rectangle &r);
 };
 
-struct point {
-	double x;
-	double y;
-    rectangle rect();
-};
-std::ifstream& operator>> (std::ifstream& in, point& x);
-struct section {
-	point* beg;
-	point* end;
-    rectangle rect();
+struct primitive {
+    virtual rectangle rect() = 0;
+    virtual inline Element type() = 0;
 };
 
-struct circle {
-	point* center;
-	double R;
-    rectangle rect();
+struct point : public primitive {
+    double x;
+    double y;
+
+    rectangle rect() override;
+    inline Element type(){
+        return ET_POINT;
+    }
+};
+
+std::ifstream &operator>>(std::ifstream &in, point &x);
+
+struct section : public primitive {
+    point *beg;
+    point *end;
+
+    rectangle rect() override;
+    inline Element type(){
+        return ET_SECTION;
+    }
+};
+
+struct circle : public primitive {
+    point *center;
+    double R;
+
+    rectangle rect() override;
+    inline Element type(){
+        return ET_CIRCLE;
+    }
 };
 
 #endif
