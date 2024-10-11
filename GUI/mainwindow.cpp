a250c353e5e8279009584890c33bb7a285fe7dc7
Tim <timon.skvor@mail.ru>
2024-10-11 20:45:50 +0300

    delete class SaveDialog

--- GUI/mainwindow.cpp
+++ GUI/mainwindow.cpp
@@ -1,44 +1,5 @@
 #include "mainwindow.h"
 
-class SaveDialog : public QDialog {
-public:
-    SaveDialog(QWidget *parent = nullptr) : QDialog(parent) {
-        setWindowTitle("Сохранение");
-
-        setStyleSheet("background-color: gray; color: black;");
-
-        QVBoxLayout *layout = new QVBoxLayout(this);
-        QLabel *label = new QLabel("Сохранить изменения?", this);
-        layout->addWidget(label);
-
-        QHBoxLayout *buttonLayout = new QHBoxLayout();
-
-        QPushButton *yesButton = new QPushButton("Да", this);
-        QPushButton *noButton = new QPushButton("Нет", this);
-        QPushButton *cancelButton = new QPushButton("Отмена", this);
-
-        yesButton->setStyleSheet("background-color: darkgray; color: black;");
-        noButton->setStyleSheet("background-color: darkgray; color: black;");
-        cancelButton->setStyleSheet("background-color: darkgray; color: black;");
-
-        buttonLayout->addWidget(yesButton);
-        buttonLayout->addWidget(noButton);
-        buttonLayout->addWidget(cancelButton);
-
-        layout->addLayout(buttonLayout);
-
-        connect(yesButton, &QPushButton::clicked, this, [this]() {
-            done(QMessageBox::Yes);
-        });
-        connect(noButton, &QPushButton::clicked, this, [this]() {
-            done(QMessageBox::No);
-        });
-        connect(cancelButton, &QPushButton::clicked, this, [this]() {
-            done(QMessageBox::Cancel);
-        });
-    }
-};
-
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow), Index(-1),save(true) {
 
     ui->setupUi(this);
