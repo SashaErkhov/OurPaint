0e555d2708984c46d80794407f267b7aefd89328
Tim <timon.skvor@mail.ru>
2024-10-20 01:50:27 +0300

    add castome window,change design

--- GUI/SaveDialog.h
+++ GUI/SaveDialog.h
@@ -11,14 +11,22 @@
 
 
 class SaveDialog : public QDialog {
+private:
+    QString file;
+    bool mousePressed;
+    QPoint mousePos;
 public:
+    //void setFile(QString File){file=File;}
     SaveDialog(QWidget *parent = nullptr) : QDialog(parent) {
-        setWindowTitle("Сохранение");
+        setWindowFlags(Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint | Qt::Tool);
+        setAttribute(Qt::WA_TranslucentBackground);
+        setWindowModality(Qt::ApplicationModal);  // Для модальности только в пределах родительского окна
 
-        setStyleSheet("background-color: gray; color: black;");
+       // setStyleSheet("background-color: gray; color: black;");
 
         QVBoxLayout *layout = new QVBoxLayout(this);
-        QLabel *label = new QLabel("Сохранить изменения?", this);
+        layout->setContentsMargins(10, 10, 10, 10);
+        QLabel *label = new QLabel("Сохранить проект?", this);
         layout->addWidget(label);
 
         QHBoxLayout *buttonLayout = new QHBoxLayout();
@@ -27,9 +35,9 @@ public:
         QPushButton *noButton = new QPushButton("Нет", this);
         QPushButton *cancelButton = new QPushButton("Отмена", this);
 
-        yesButton->setStyleSheet("background-color: darkgray; color: black;");
-        noButton->setStyleSheet("background-color: darkgray; color: black;");
-        cancelButton->setStyleSheet("background-color: darkgray; color: black;");
+      //  yesButton->setStyleSheet("background-color: darkgray; color: black;");
+      //  noButton->setStyleSheet("background-color: darkgray; color: black;");
+       // cancelButton->setStyleSheet("background-color: darkgray; color: black;");
 
         buttonLayout->addWidget(yesButton);
         buttonLayout->addWidget(noButton);
@@ -47,6 +55,39 @@ public:
             done(QMessageBox::Cancel);
         });
     }
+protected:
+    void paintEvent(QPaintEvent *event) override {
+        QPainter painter(this);
+        painter.setRenderHint(QPainter::Antialiasing);
+        painter.setBrush(QColor(50, 50, 50, 240));
+        painter.setPen(Qt::NoPen);
+        painter.drawRoundedRect(rect(), 10, 10);
+    }
+
+    void mousePressEvent(QMouseEvent *event) override { // Нажатие
+        if (event->button() == Qt::LeftButton) {
+            setCursor(Qt::SizeAllCursor);
+            mousePressed = true;
+            mousePos = event->pos();
+            event->accept();
+        }
+    }
+
+    void mouseMoveEvent(QMouseEvent *event) override { // Перемещение
+        if (mousePressed) {
+            move(event->globalPos() - mousePos);
+            event->accept();
+        }
+    }
+
+    void mouseReleaseEvent(QMouseEvent *event) override { // Отпускание
+        if (event->button() == Qt::LeftButton) {
+            setCursor(Qt::ArrowCursor);
+            mousePressed = false;
+            event->accept();
+        }
+    }
+
 };
 
 
