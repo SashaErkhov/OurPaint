45facf3f149a2e5561f901f3d02cc3b5df5e4ac5
Tim <timon.skvor@mail.ru>
2024-10-20 15:21:59 +0300

    add signal

--- GUI/WindowServer.h
+++ GUI/WindowServer.h
@@ -12,6 +12,20 @@
 
 class WindowServer : public QWidget{
 Q_OBJECT
+private:
+    QLineEdit *lineEdit;
+signals:
+    void textEnter(const QString &text);
+
+private slots:
+    void OkClicked() {
+        QString text = lineEdit->text();
+        if(!text.isEmpty()) {
+            emit textEnter(text);
+        }
+        close();
+    }
+public:
     WindowServer(QWidget *parent = nullptr) : QWidget(parent){
         setWindowFlags(Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint | Qt::Tool);
         setAttribute(Qt::WA_TranslucentBackground);
@@ -21,14 +35,23 @@ Q_OBJECT
         QVBoxLayout *layout = new QVBoxLayout(this);
         layout->setContentsMargins(10, 10, 10, 10);
 
-        QLineEdit line(parent);
+        lineEdit = new QLineEdit(this);
+        layout->addWidget(lineEdit);
+
         QPushButton *okButton = new QPushButton("OK", this);
-        connect(okButton, &QPushButton::clicked, this, &WindowServer::close);
+        connect(okButton, &QPushButton::clicked, this, &WindowServer::OkClicked);
         layout->addWidget(okButton);
 
         setLayout(layout);
         resize(300, 150);
     }
-
+protected:
+    void paintEvent(QPaintEvent *event) override {
+        QPainter painter(this);
+        painter.setRenderHint(QPainter::Antialiasing);
+        painter.setBrush(QColor(50, 50, 50, 240));
+        painter.setPen(Qt::NoPen);
+        painter.drawRoundedRect(rect(), 10, 10);
+    }
 };
 #endif //OURPAINT_WINDOWSERVER_H
