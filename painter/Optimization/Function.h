d155c429f07a79ae201fbb9df400878a0d0c7595
Ibragim Oskanov <Oskanov-ibragim.06@mail.ru>
2024-10-20 23:26:09 +0300

    realize task methods

--- painter/Optimization/Function.h
+++ painter/Optimization/Function.h
@@ -1,12 +1,16 @@
 #ifndef FUNCTION_H
 #define FUNCTION_H
-#define type double // TODO
+#define type double* // TODO
 
 class Function {
 public:
     virtual ~Function() = default;
-    virtual bool hasDeriv() = 0;
-    virtual type getDeriv(type) = 0;
+    virtual bool hasDeriv() const = 0;
+    virtual bool hasSecondDeriv() const = 0;
+    virtual double getDeriv(size_t varIndex) = 0;
+    virtual double getSecondDeriv(size_t varIndex) = 0;
+    virtual size_t inputDimension() const = 0;
+    virtual size_t outputDimension() const = 0;
     virtual type operator()(type) = 0;
 };
 
--- painter/Optimization/Task.h
+++ painter/Optimization/Task.h
@@ -1,6 +1,6 @@
 #ifndef TASK_H
 #define TASK_H
-#define type double // TODO
+#define type double* // TODO
 
 #include "../../Matrix/Matrix.h"
 #include "Function.h"
@@ -12,13 +12,69 @@ class Task {
 public:
     Task(Function* function) : function(function) {};
 
-    bool hasGrad() const {};
-    bool hasJacob() const {};
-    bool hasHess() const {};
+    bool hasGrad() const {
+        return function->hasDeriv();
+    };
 
-    std::vector<type> getGrad() const {};
-    Matrix<type> getJacob() const {};
-    Matrix<type> getHess() const {};
+    bool hasJacob() const {
+        return function->hasDeriv();
+    };
+
+    bool hasHess() const {
+        return function->hasSecondDeriv();
+    };
+
+
+    std::vector<double> getGrad() const {
+        if (!hasGrad()) {
+            throw std::runtime_error("Gradient not available");
+        }
+
+        size_t dim = function->inputDimension();
+
+        std::vector<double> gradient;
+
+        for (size_t i = 0; i < dim; ++i) {
+            gradient.push_back(function->getDeriv(i));
+        }
+
+        return gradient;
+    };
+
+    Matrix<double> getJacob() const {
+        if (!hasJacob()) {
+            throw std::runtime_error("Jacobin not available");
+        }
+
+        size_t inputDim = function->inputDimension();
+        size_t outputDim = function->outputDimension();
+        Matrix<double> jacobian(outputDim, inputDim);
+
+        for (size_t i = 0; i < outputDim; ++i) {
+            for (size_t j = 0; j < inputDim; ++j) {
+                jacobian.setElement(i, j, function->getDeriv(i));  // calc derivative by i function and by j var
+            }
+        }
+
+        return jacobian;
+    };
+
+    Matrix<double> getHess() const {
+        if (!hasHess()) {
+            throw std::runtime_error("Hessian not available");
+        }
+
+        size_t inputDim = function->inputDimension();
+        Matrix<double> hessian(inputDim, inputDim);
+
+        for (size_t i = 0; i < inputDim; ++i) {
+            for (size_t j = 0; j < inputDim; ++j) {
+                hessian.setElement(i, j, function->getSecondDeriv(j));  // calc second derivative by i var and by j var
+            }
+        }
+
+        return hessian;
+    };
 };
 
 #endif // TASK_H
